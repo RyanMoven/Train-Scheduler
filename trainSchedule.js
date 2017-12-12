426abf04505c97729fd1b8e43df5bf9c72b62cb4
RyanMoven <ryanmoven@gmail.com>
2017-12-12 10:10:25 -0500

    Initialized Firebase

--- trainSchedule.js
+++ trainSchedule.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
     console.log( "ready!" );
-});
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAbcO9fbolP6oKi8EmZKL56VK8fyT3Sc6o",
+    authDomain: "train-schedule-database.firebaseapp.com",
+    databaseURL: "https://train-schedule-database.firebaseio.com",
+    projectId: "train-schedule-database",
+    storageBucket: "train-schedule-database.appspot.com",
+    messagingSenderId: "281568785753"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+// form initializer
+var trainName = ""
+var destination = ""
+var firstTrainTime = ""
+var frequency = ""
+
+
+
+
+
+
+
+
+}); // end of document.ready
